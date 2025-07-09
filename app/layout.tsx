76aa1ae3ce1f39595aed2f19cf035c18d2272653
Rodolpho-Alvim <rodolpho_360jac@hotmail.com>
2025-07-09 17:23:11 -0300

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Tarkov Kappa",
+  description: "Creado por Rodolpho Alvim",
+  generator: "Rodolpho-Alvim",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
